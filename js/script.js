126f8686720033ad4283e0bc192391fa72efe88b
Aneckon <83111616+Aneckon@users.noreply.github.com>
2021-11-09 18:56:04 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 var swiper = new Swiper(".mySwiper", {
-  direction: "vertical",
-  slidesPerView: 1,
-  spaceBetween: 30,
-  mousewheel: true,
+  autoplay: {
+    delay: 5000,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -11,9 +10,29 @@ var swiper = new Swiper(".mySwiper", {
 
 $(document).ready(function() {
   $('.header__burder-img').click(function() {
+      $('.header__block').toggleClass('menu__open');
       $('.header__list').toggleClass('menu__open');
       $('.header__tel').toggleClass('menu__open');
       $('.header__burder-img').toggleClass('menu__img');
       $('body').toggleClass('fixed');
   });
 });
+
+$(document).ready(function() {
+  $('.location__header').click(function() {
+      $('.location__title').toggleClass('title_active');
+      $('.location__title').toggleClass('title_close');
+      $('.location__adres').toggleClass('none');
+  });
+});
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        $(this).next().slideToggle()
+    });
+}
